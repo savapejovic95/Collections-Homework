946548c4a1a9391d822a4e8546405c9c648af9bd
savapejovic95 <sava.pejovic@yahoo.com>
2016-06-16 14:28:52 +0200

    Implementiran program za prikazivanje frenkvencije svih bigrama stringa: abbcceeeeeeabcc

--- Kolekcije/src/bigram/Bigram.java
+++ Kolekcije/src/bigram/Bigram.java
@@ -1,7 +1,28 @@
 package bigram;
 
+import java.util.Map.Entry;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Bigram {
 	public static void main(String[] args) {
+		String s = "abbcceeeeeeabcc";
+		Map<String, Integer> mapa = new HashMap<>();
 
+		for (int i = 0; i < s.length() - 1; i++) {
+			String bigram = s.substring(i, i+2);
+			if(mapa.containsKey(bigram)){
+				mapa.put(bigram, mapa.get(bigram) + 1);
+			}
+			else{
+				mapa.put(bigram, 1);
+			}
+		}
+		Iterator i = mapa.entrySet().iterator();
+		while (i.hasNext()) {
+			Map.Entry<String, Integer> entry = (Entry<String, Integer>) i.next();
+			System.out.println(entry.getKey() + " = " + entry.getValue());
+		}
 	}
 }
